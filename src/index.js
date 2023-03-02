f63af712ca121f0c8858dd01bbaf63a7e1c7447e
chvg1968 <conradovilla.gmail.com>
2023-03-01 20:51:44 -0500

    Update list countries

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
-import { debounce } from "lodash";
+// import { debounce } from "lodash";
 import { fetchCountryData } from "./fetchCountries.js";
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-
+import { debounce } from 'lodash';
 
 const countryInp = document.getElementById("country-inp");
-const result = document.getElementById("result");
+const suggestionsList = document.createElement("ul");
+countryInp.after(suggestionsList);
 const DEBOUNCE_DELAY = 300;
 
 const handleInput = debounce(() => {
   const countryName = countryInp.value.trim();
   if (countryName.length === 0) {
     Notify.failure('The input field cannot be empty');
+    suggestionsList.innerHTML = "";
   } else {
     fetchCountryData(countryName)
       .then((data) => {
@@ -52,6 +54,39 @@ const handleInput = debounce(() => {
       .catch(() => {
         Notify.failure('Please enter a valid country name');
       });
+
+      fetch(`https://restcountries.com/v3.1/name/${countryName}`)
+  .then((response) => response.json())
+  .then((data) => {
+    suggestionsList.innerHTML = "";
+    const numCountries = data.length;
+    if (numCountries > 10) {
+      const tooManyMatches = document.createElement("li");
+      Notify.failure(`Too many matches found. Please be more specific.`);
+      suggestionsList.appendChild(tooManyMatches);
+    } else if (numCountries >= 2 && numCountries <= 10) {
+      data.forEach((country) => {
+        const suggestionItem = document.createElement("li");
+        const countryName = country.name.common;
+        const flagUrl = country.flags.svg;
+        const flagImg = document.createElement("img");
+        flagImg.src = flagUrl;
+        flagImg.alt = countryName + " flag";
+        flagImg.width = 20;
+        flagImg.height = 20;
+        suggestionItem.appendChild(flagImg);
+        suggestionItem.appendChild(document.createTextNode(countryName));
+        suggestionsList.append(suggestionItem);
+      });
+      const noMatches = document.createElement("li");
+      Notify.failure("Oops there is no country with that name");
+      suggestionsList.appendChild(noMatches);
+    }
+  })
+  .catch(() => {
+    suggestionsList.innerHTML = "";
+  });
+
   }
 }, DEBOUNCE_DELAY);
 
